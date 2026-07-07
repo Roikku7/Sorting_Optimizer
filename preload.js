f672b0bd30487cb75b0c8be18dcda3f73c4f6fdb
Roikku7 <lokick17@gmail.com>
2026-07-07 20:31:57 +0200

    feat: settings persistence IPC (get-settings/save-settings), pass settings to analysis

--- preload.js
+++ preload.js
@@ -3,5 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld("electronAPI", {
   selectJsonFile: () => ipcRenderer.invoke("select-json-file"),
   runAnalysis: (filePath) => ipcRenderer.invoke("run-analysis", filePath),
-  getIconPath: (name) => ipcRenderer.invoke("get-icon-path", name)
+  getIconPath: (name) => ipcRenderer.invoke("get-icon-path", name),
+  getSettings: () => ipcRenderer.invoke("get-settings"),
+  saveSettings: (settings) => ipcRenderer.invoke("save-settings", settings)
 });
